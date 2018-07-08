488e961c0847e3b4d8452dc5628e97fa2c8c615f
Jhonathan A. Salazar <salajhon69@gmail.com>
2018-07-08 18:27:26 -0500

    Initial Commit

---
+++ AR Drawing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AR Drawing
+//
+//  Created by Jhonathan A. Salazar on 7/8/18.
+//  Copyright © 2018 Jhonathan A. Salazar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
